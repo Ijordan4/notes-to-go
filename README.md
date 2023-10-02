ff974270d753564e7bea103a34c460294c7f646c
ijordan4 <isaiahjordan200@gmail.com>
2023-10-02 00:37:55 -0400

    fixed

--- README.md
+++ README.md
@@ -1,13 +1,1 @@
-# notes-to-go
-## Description
-"Notes To Go" is a simple note-taking application designed to help you quickly jot down and organize your thoughts, ideas, and important information on the go. Whether you're in a meeting, brainstorming session, or just need a place to capture your thoughts, this app has you covered.
-
-## Features
-Create and Edit Notes: Easily create new notes or edit existing ones.
-Organize Your Notes: Group and categorize your notes with tags and labels.
-Search and Filter: Quickly find specific notes with a powerful search and filter functionality.
-Sync Across Devices: Sync your notes across multiple devices to access them wherever you go.
-Offline Access: Access your notes even when you're offline.
-Security: Keep your notes safe with password protection or biometric authentication.
-Customization: Customize the app's appearance and settings to suit your preferences.
-Export and Share: Share your notes with others or export them in various formats.
+# note-taker
--- server.js
+++ server.js
@@ -1,106 +1,56 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-
-// create instance of express
+const PORT = process.env.PORT || 3001;
+const express = require("express");
 const app = express();
+const path = require("path");
+const fs = require("fs");
+const everyNote = require("./db/db.json");
 
-// Set up middleware
-app.use(express.static('public'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
 
-// define routes to handle requests and for the server
-// create html route for index.html and notes.html
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+app.get("/api/notes", (req, res) => {
+    res.json(everyNote.slice(1));
 });
 
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/notes.html'));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-const dbFilePath = path.join(__dirname, 'db.json');
-
-// create api route to get all notes from data from db.json and return as JSON
-app.get('/api/notes', (req, res) => {
-  const notes = JSON.parse(fs.readFileSync(dbFilePath, 'utf8'));
-  res.json(notes);
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-app.post('/api/notes', (req, res) => {
-  const notes = JSON.parse(fs.readFileSync(dbFilePath, 'utf8'));
-  const newNote = req.body;
-  newNote.id = generateUniqueId();
-  notes.push(newNote);
-  fs.writeFileSync(dbFilePath, JSON.stringify(notes));
-  res.json(newNote);
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-app.delete('/api/notes/:id', (req, res) => {
-  const notes = JSON.parse(fs.readFileSync(dbFilePath, 'utf8'));
-  const noteId = req.params.id;
-  const updatedNotes = notes.filter((note) => note.id !== noteId);
-  fs.writeFileSync(dbFilePath, JSON.stringify(updatedNotes));
-  res.sendStatus(204);
+function createNote (body, noteArr) {
+    const note = body;
+    if(!Array.isArray(noteArr)) {
+        noteArr = [];
+    };
+
+    if(noteArr.length === 0) {
+        noteArr.push(0);
+    };
+
+    body.id = noteArr[0];
+    noteArr[0]++;
+
+    noteArr.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, "./db/db.json"),
+        JSON.stringify(noteArr, null, 2)
+    );
+    return note;
+};
+
+app.post("/api/notes", (req, res) => {
+    const note = createNote(req.body, everyNote);
+    res.json(note);
 });
 
-// connect back end to front end
-const handleNoteSave = () => {
-  const newNote = {
-    title: noteTitle.value,
-    text: noteText.value,
-  };
-  saveNote(newNote).then(() => {
-    getAndRenderNotes();
-    renderActiveNote();
-  })
-  .catch((error) => {
-    console.error('Error saving note:', error);
-  });
-};
-
-const handleNoteDelete = (e) => {
-  e.stopPropagation();
-
-  const note = e.target;
-  const noteId = JSON.parse(note.parentElement.getAttribute('data-note')).id;
-
-  if (activeNote.id === noteId) {
-    activeNote = {};
-  }
-
-  deleteNote(noteId).then(() => {
-    getAndRenderNotes();
-    renderActiveNote();
-  })
-  .catch((error) => {
-    console.error('Error deleting note:', error);
-  });
-};
-
-const handleNoteView = (e) => {
-  e.preventDefault();
-  activeNote = JSON.parse(e.target.parentElement.getAttribute('data-note'));
-  renderActiveNote();
-};
-
-const handleNewNoteView = (e) => {
-  activeNote = {};
-  renderActiveNote();
-};
-
-const generateUniqueId = () => {
-  // Generate a unique ID for the note
-  // You can use libraries like uuid or shortid for this purpose
-  // Example: const uuid = require('uuid');
-  //          const id = uuid.v4();
-  // For simplicity, let's assume generateUniqueId() returns a unique string
-  // Implement your own logic for generating unique IDs if needed
-  return 'unique-id';
-};
-
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+    console.log(`Server available at localhost ${PORT}`);
 });
