438bf67748b30c09c115f8c5031abcca16b62c5a
Ijordan4 <124106922+Ijordan4@users.noreply.github.com>
2023-10-03 09:01:03 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,54 +3,63 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const fs = require("fs");
-const everyNote = require("./db/db.json");
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+
+let everyNote = require("./db/db.json");
+
+// Use a middleware function to set common headers for all routes
+app.use((req, res, next) => {
+  res.setHeader("Content-Type", "application/json");
+  next();
+});
+
+
 app.get("/api/notes", (req, res) => {
-    res.json(everyNote.slice(1));
+  res.json(everyNote);
 });
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-function createNote (body, noteArr) {
-    const note = body;
-    if(!Array.isArray(noteArr)) {
-        noteArr = [];
-    };
+function createNote(body) {
+  const note = body;
+  if (!Array.isArray(everyNote)) {
+    everyNote = [];
+  }
 
-    if(noteArr.length === 0) {
-        noteArr.push(0);
-    };
+  if (everyNote.length === 0) {
+    everyNote.push({ id: 0 });
+  }
 
-    body.id = noteArr[0];
-    noteArr[0]++;
+  body.id = everyNote[0].id;
+  everyNote[0].id++;
 
-    noteArr.push(note);
-    fs.writeFileSync(
-        path.join(__dirname, "./db/db.json"),
-        JSON.stringify(noteArr, null, 2)
-    );
-    return note;
-};
+  everyNote.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, "./db/db.json"),
+    JSON.stringify(everyNote, null, 2)
+  );
+  return note;
+}
 
 app.post("/api/notes", (req, res) => {
-    const note = createNote(req.body, everyNote);
-    res.json(note);
+  const note = createNote(req.body);
+  res.json(note);
 });
 
 app.listen(PORT, () => {
-    console.log(`Server available at localhost ${PORT}`);
+  console.log(`Server available at http://localhost:${PORT}`);
 });
